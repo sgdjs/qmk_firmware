c2ed6ab622595d77f61cae3eb02081fbbb55d6b1
Simon <simon.gdjs@gmail.com>
2017-02-12 11:36:25 -0500

    Put AltGr in Shift on Lower/Raise layers

--- keyboards/planck/keymaps/optimo/keymap.c
+++ keyboards/planck/keymaps/optimo/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = {
   {KC_DEL,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
-  {_______, KC_PIPE, KC_PLUS, KC_UNDS, KC_DQUO, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______},
+  {KC_RALT, KC_PIPE, KC_PLUS, KC_UNDS, KC_DQUO, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RALT},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = {
   {KC_DEL,  KC_F7,   KC_F5,  KC_F3,   KC_F1,   KC_F9,    KC_F8,   KC_F10,  KC_F2,   KC_F4,   KC_F6,   _______},
   {KC_GRV,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
-  {_______, KC_BSLS, KC_EQL, KC_MINS, KC_QUOT, KC_LBRC,  KC_RBRC, _______, _______, KC_F11,  KC_F12,  _______},
+  {KC_RALT, KC_BSLS, KC_EQL, KC_MINS, KC_QUOT, KC_LBRC,  KC_RBRC, _______, _______, KC_F11,  KC_F12,  KC_RALT},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
