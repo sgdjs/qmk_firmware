92845d71c859a750a8c0033b937f0526919cdfb8
Simon <simon.gdjs@gmail.com>
2017-02-12 14:54:28 -0500

    Add keys to Adjust layer:
    * Cut-Copy-Paste
    * Mute
    * Media previous

--- keyboards/planck/keymaps/optimo/keymap.c
+++ keyboards/planck/keymaps/optimo/keymap.c
@@ -109,9 +109,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = {
   {KC_DEL,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, MU_ON,   MU_OFF,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   _______},
-  {KC_INS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,  KC_PMNS},
-  {_______, _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS},
-  {_______, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PENT}
+  {KC_INS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,  KC_PMNS},
+  {_______, KC_CUT,  KC_COPY, KC_PASTE, KC__MUTE, KC_VOLD, KC_VOLU, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS},
+  {KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PENT}
 },
 
 /* Colemak
