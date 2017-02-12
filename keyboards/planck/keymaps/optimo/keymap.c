7081bc00b162434cad5d160cbe6042414b5a154c
Simon <simon.gdjs@gmail.com>
2017-02-12 11:18:56 -0500

    Update F1-12 position for Optimo+, put PageUp/Down in the Adjust layer

--- keyboards/planck/keymaps/optimo/keymap.c
+++ keyboards/planck/keymaps/optimo/keymap.c
@@ -90,9 +90,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
-  {_______, KC_BSLS, KC_EQL, KC_MINS, KC_QUOT, KC_LBRC,  KC_RBRC, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
+  {_______, KC_F7,   KC_F5,  KC_F3,   KC_F1,   KC_F9,    KC_F8,   KC_F10,  KC_F2,   KC_F4,   KC_F6,   _______},
+  {KC_GRV,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  {_______, KC_BSLS, KC_EQL, KC_MINS, KC_QUOT, KC_LBRC,  KC_RBRC, _______, _______, KC_F11,  KC_F12,  _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, KC_HOME, KC_UP,   KC_END,  _______, MU_ON,   MU_OFF,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   _______},
-  {_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,  KC_PMNS},
+  {_______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, MU_ON,   MU_OFF,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   _______},
+  {_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,  KC_PMNS},
   {_______, _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS},
   {_______, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PENT}
 },
