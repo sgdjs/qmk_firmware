856d1eb0fbf08b7d283bcc6fb1f71965ffd16404
Simon <simon.gdjs@gmail.com>
2017-02-12 11:38:33 -0500

    Put Insert key on Adjust layer

--- keyboards/planck/keymaps/optimo/keymap.c
+++ keyboards/planck/keymaps/optimo/keymap.c
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = {
   {KC_DEL,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, MU_ON,   MU_OFF,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   _______},
-  {_______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,  KC_PMNS},
+  {KC_INS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,  KC_PMNS},
   {_______, _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS},
   {_______, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PENT}
 },
